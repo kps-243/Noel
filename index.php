f976838c07916477a78929c15606c855a726ab27
AlexandreBasei <alexandreacevedobasei@gmail.com>
2022-12-01 11:48:33 +0100

    changement du nom du cpt

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php get_header(); ?>
 
 	<main>
-		Contenu
+		<?php
+		$args = array(
+		'post_status' => 'publish',
+		'post_per_page' => 'cpt_GAB',
+		'post_per_page' => -1
+		);
+
+		$recette = Wp_Query($args);
+
+		if($recettes -> have_posts()):
+			while ($recettes -> have_posts()): $recettes -> the_post();
+		?>
+
+		<?php
+		the_title();
+	?>
 	</main>
 
 <?php get_footer(); ?>
