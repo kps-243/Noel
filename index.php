8c2d520f4d72ffac8e9c5f32e106e57a4aaef89b
AlexandreBasei <alexandreacevedobasei@gmail.com>
2022-12-01 12:01:33 +0100

    modif du fichier index.php pour afficher le cpt

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 
 	<main>
 		<?php
-		$args = array(
-		'post_status' => 'publish',
-		'post_per_page' => 'cpt_GAB',
-		'post_per_page' => -1
-		);
+			$args = array(
+			'post_status' => 'publish',
+			'post_per_page' => 'cpt_recette',
+			'post_per_page' => -1
+			);
 
-		$recette = Wp_Query($args);
+			$recette = Wp_Query($args);
 
-		if($recettes -> have_posts()):
-			while ($recettes -> have_posts()): $recettes -> the_post();
+			if($recettes -> have_posts()):
+				while ($recettes -> have_posts()): $recettes -> the_post();
 		?>
 
 		<?php
-		the_title();
-	?>
+			the_title();
+		?>
 	</main>
 
-<?php get_footer(); ?>
+<?php get_footer(); ?>;
