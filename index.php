f9c0925f1f332ac720483886b79b681111bdd98f
AlexandreBasei <alexandreacevedobasei@gmail.com>
2022-12-03 16:39:50 +0100

    ajout de la première version du jeu du sapin sans les fils de connexion

--- index.php
+++ index.php
@@ -1,22 +1,53 @@
 <?php get_header(); ?>
 
+<?php
+    
+    $args = array(
+        'post_status' => 'publish',
+        'post_type' => 'cpt_recette',
+        'posts_per_page' => -1
+    );
+    
+    $recettes = new WP_Query($args);
+    
+    if ($recettes -> have_posts()):
+        while($recettes -> have_posts()): $recettes -> the_post();
+?>
+<!--<pre>-->
+<!--    <?php  var_dump($recettes); ?>-->
+<!--</pre>-->
+<?php
+        endwhile; //while($recettes -> have_posts()): $recettes -> the_post();
+    endif; //if ($recettes -> have_posts()):
+?>
+
+
 	<main>
-		<?php
-			$args = array(
-			'post_status' => 'publish',
-			'post_per_page' => 'cpt_recette',
-			'post_per_page' => -1
-			);
-
-			$recette = Wp_Query($args);
-
-			if($recettes -> have_posts()):
-				while ($recettes -> have_posts()): $recettes -> the_post();
-		?>
-
-		<?php
-			the_title();
-		?>
+	    
+	    <?php
+	        $gout = get_field('gout');
+	        echo '<pre><?php var_dump($gout) ?></pre>';
+	    ?>
+	    
+	    <h1><?php the_title(); ?></h1>
+	    
+	    <?php if (get_field('preparation')): ?>
+		Préparation : <?php the_field('preparation')?> minutes<br>
+		<?php endif; ?>
+		
+		<?php if (get_field('cuisson')): ?>
+		Cuisson : <?php the_field('cuisson')?> minutes<br>
+		<?php endif; ?>
+		
+		<?php if (get_field('ingredients')): ?>
+		<strong>Ingrédients :</strong><br>
+		<p><?php the_field('ingredients')?></p>
+		<?php endif; ?>
+		
+		<?php if (get_field('recette')): ?>
+		<strong>Recette :</strong><br>
+		<section><?php the_field('recette')?></section>
+		<?php endif; ?>
 	</main>
 
-<?php get_footer(); ?>;
+<?php get_footer(); ?>
