677eb40933c173ef464577c6c29c5ef02f4aef83
AlexandreBasei <alexandreacevedobasei@gmail.com>
2022-12-09 11:15:11 +0100

    mise à jour du jeu : génération aléatoire et victoire

--- jeusapin.js
+++ jeusapin.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     'use strict';
 
+    const div = document.getElementById("maDiv");
+    const div2 = document.getElementById("maDiv2");
+    const div3 = document.getElementById("maDiv3");
+    const div4 = document.getElementById("maDiv4");
+
     function alea(tab) { //Mélange d'un tableau
         let i, j, tmp;
         for (i = tab.length - 1; i > 0; i--) {
@@ -15,15 +20,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function prefix(tab, x) {//Ajout du préfixe "#btn" ou "#btn2" devant chaque valeur du tableau
         let i;
-        let tab2 = [5,5,5,5];
+        let tab2 = [5, 5, 5, 5];
         if (x == 0) {
-            for (i = 0; i < 4; i++){
-                tab2[i] = "#btn"+tab[i];
+            for (i = 0; i < 4; i++) {
+                tab2[i] = "#btn" + tab[i];
             }
         }
-        else{
-            for (i = 0; i < 4; i++){
-                tab2[i] = "#btn2"+tab[i];
+        else {
+            for (i = 0; i < 4; i++) {
+                tab2[i] = "#btn2" + tab[i];
             }
         }
         return tab2;
@@ -32,16 +37,59 @@ document.addEventListener('DOMContentLoaded', function () {
     function couleur() { //Association de deux boutons par la couleur
         btn1.style.backgroundColor = 'red';
         btn21.style.backgroundColor = 'red';
+        var couleur1 = 'red';
 
         btn2.style.backgroundColor = 'blue';
         btn22.style.backgroundColor = 'blue';
+        var couleur2 = 'blue';
 
         btn3.style.backgroundColor = 'green';
         btn23.style.backgroundColor = 'green';
+        var couleur3 = 'green';
 
         btn4.style.backgroundColor = 'yellow';
         btn24.style.backgroundColor = 'yellow';
+        var couleur4 = 'yellow';
     }
+
+    //Création d'un fil qui suit la souris
+    function fil(fil, btn) {
+        let dernierX = 0;
+        let dernierY = 0;
+        fil.style.backgroundColor = btn.style.backgroundColor;
+
+        document.addEventListener('mousemove', (event) => {
+            //On détecte si l'utilisateur a cliqué sur un bouton
+            if (!dessin) {
+                return;
+            }
+            
+            //On empêche la largeur du fil de dépasser la souris quand on se déplace à droite
+            fil.style.width = Math.min(event.clientX, fil.offsetWidth) + 'px';
+    
+            //Même chose pour le déplacement vers la gauche
+            fil.style.width = Math.max(event.clientX, fil.offsetLeft) + 'px';
+    
+            //Détection du déplacement horizontal de la souris
+            const actuelX = event.clientX;
+            if (actuelX < dernierX) { //Vers la gauche
+                fil.style.width = (fil.offsetWidth - 5) + 'px'; //Réduction largeur du fil quand la souris se déplace vers la gauche
+            }
+            if (actuelX > dernierX) { //Vers la droite
+                fil.style.width = (fil.offsetWidth + 5) + 'px'; //Augmentation quand elle se déplace vers la droite
+            }
+            dernierX = actuelX;
+    
+            //Détection du déplacement vertical de la souris
+            const actuelY = event.clientY;
+            if (actuelY !== dernierY) {
+                // alert('vertical');
+                fil.style.top = event.clientY + 40 + 'px';
+            }
+            // dernierX = actuelY;
+        });
+    }
+    
 
     //Déclaration de deux tableaux
     var tab = [1, 2, 3, 4];
@@ -75,16 +123,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var actif = 0;
     var victoire = 0;
+    var dessin = false;
 
     btn1.addEventListener('click', () => {
         actif = 1;
+        dessin = true;
+        document.style.cursor = 'red';
+        fil(div, btn1);
     });
 
     btn21.addEventListener('click', () => {
-        if (actif == 1){
+        if (actif == 1) {
             btn1.style.border = '0px';
             btn21.style.border = '0px';
             victoire++;
+            dessin = false;
             actif = 0;
         }
         if (victoire == 4) {
@@ -94,13 +147,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     btn2.addEventListener('click', () => {
         actif = 2;
+        dessin = true;
+        fil(div2, btn2);
+        
     });
 
     btn22.addEventListener('click', () => {
-        if (actif == 2){
+        if (actif == 2) {
             btn2.style.border = '0px';
             btn22.style.border = '0px';
             victoire++;
+            dessin = false;
             actif = 0;
         }
         if (victoire == 4) {
@@ -110,13 +167,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     btn3.addEventListener('click', () => {
         actif = 3;
+        dessin = true;
+        fil(div3, btn3);
     });
 
     btn23.addEventListener('click', () => {
-        if (actif == 3){
+        if (actif == 3) {
             btn3.style.border = '0px';
             btn23.style.border = '0px';
             victoire++;
+            dessin = false;
             actif = 0;
         }
         if (victoire == 4) {
@@ -126,13 +186,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     btn4.addEventListener('click', () => {
         actif = 4;
+        dessin = true;
+        fil(div4, btn4);
     });
 
     btn24.addEventListener('click', () => {
-        if (actif == 4){
+        if (actif == 4) {
             btn4.style.border = '0px';
             btn24.style.border = '0px';
             victoire++;
+            dessin = false;
             actif = 0;
         }
         if (victoire == 4) {
