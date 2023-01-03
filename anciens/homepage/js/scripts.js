7c9ac0ca9c8f3e4f234899c73649ca3da1bf0413
Anna ROBILLARD <22101583@ad.ens.iut-velizy.uvsq.fr>
2023-01-03 11:30:57 +0100

    Voici tout les dossiers triés

---
+++ anciens/homepage/js/scripts.js
@@ -0,0 +1,130 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // git init
+    // git remote add origin https://github.com/kps-243/Noel.git
+    // git checkout -b nom_de_ma_branch_nouvelle
+    // git add .
+    // git commit -m "Test ajout de memory"
+    // git push (--set-upstream origin memory)
+
+   
+   
+   
+    var element = document.scrollingElement || document.documentElement;
+    element.addEventListener('wheel', transformScroll);
+
+
+
+
+    // CHANGEMENT LEFT ANIMAUX QUAND PORTRAIT
+
+    let h_img = 630;
+    let w_img = 1120
+
+    let largeur = window.innerWidth;
+    let hauteur = window.innerHeight;
+
+
+    window.onresize = handle;
+
+    function handle(evnt){
+        alert("l'événement resize a été déclenché." );
+        return true;
+      }
+
+      
+    if (h_img*largeur/w_img > hauteur) {
+        console.log("format PORTAIT");
+
+        let widthImageNow = w_img*hauteur/h_img;
+        let moreOnTheLeft = (largeur-widthImageNow)/2;
+
+
+        // Pour les animaux
+        const animaux = document.querySelectorAll('.animaux');
+        animaux.forEach(animal => {
+
+            let aCharacter = document.getElementById(animal.id);
+            // console.log(aCharacter);
+    
+
+            const cssObj = window.getComputedStyle(aCharacter, null);
+            let left = cssObj.getPropertyValue("left");
+            left.slice(0, -2);          //Retirer le "px" pour pouvoir faire les calculs
+            left = parseInt(left);
+
+            aCharacter.style.left = left+ moreOnTheLeft +"px";
+        })
+
+
+
+        // Pour les lieux
+        const lieux = document.querySelectorAll('.lieux');
+        lieux.forEach(lieu => {
+
+            let aPlace = document.getElementById(lieu.id);
+            // console.log(aPlace);
+    
+
+            const cssObj2 = window.getComputedStyle(aPlace, null);
+            let left = cssObj2.getPropertyValue("left");
+            left.slice(0, -2);          //Retirer le "px" pour pouvoir faire les calculs
+            left = parseInt(left);
+
+            aPlace.style.left = left+ moreOnTheLeft +"px";
+        })
+
+
+    } else {
+        console.log("format PAYSAGE");
+    }
+
+
+
+
+
+
+
+    // TEXTES LISTENERS
+    const listenersAbris = ["grotte","musee","igloo","maison","sapin","palais","royaume","tour"];
+    listenersAbris.forEach(abri => {
+
+        document.getElementById(abri).addEventListener('mousemove', function(event) {
+            custom(event,'text-'+abri);
+        });
+        document.getElementById(abri).addEventListener('mouseleave', function(event) {
+            leave('text-'+abri);
+        });
+    })
+
+}, false)
+
+
+  
+
+////////////// FONCTIONS /////////////
+
+function transformScroll(event) {
+    if (!event.deltaY) {
+      return;
+    }
+    let augmente = 300;
+
+    if (event.deltaY < 0)  augmente = -augmente;
+    event.currentTarget.scrollLeft += event.deltaY + event.deltaX + augmente;
+
+  }
+
+
+function custom(event,element){
+    let el = document.getElementById(element);
+    el.style.display = "block";
+
+    el.style.top = event.clientY + "px";
+    el.style.left = event.clientX + 50 + document.scrollingElement.scrollLeft + "px";
+}
+
+function leave(element){
+    let el = document.getElementById(element);
+    el.style.display = "none";
+}
---
+++ js/scripts-memory.js
@@ -0,0 +1,103 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // git init
+    // git remote add origin https://github.com/kps-243/Noel.git
+    // git checkout -b nom_de_ma_branch_nouvelle
+    // git add .
+    // git commit -m "Test ajout de memory"
+    // git push (--set-upstream origin memory)
+
+    // https://3dtransforms.desandro.com/card-flip
+
+
+    const cards = document.querySelectorAll('.card');
+
+    let duo= [];
+    let paires=0;
+    let retournees = [];
+
+    const pairesMax = cards.length/2;
+
+    // Il faut initialiser les URLs/ paires randoms
+    randomAssociations(cards);
+
+  
+
+    if (paires!=pairesMax){
+        // Gerer les clics sur les cartes
+        cards.forEach(carte => {
+            carte.addEventListener('click', function handleClick(event) {
+
+                // Conditions cliquer sur une carte
+                if (carte.id!=duo[0] && !retournees.includes(carte.id) && carte.id!=duo[1]){
+                    carte.classList.toggle('is-flipped');
+
+                        // Si deux cartes sont retournées
+                        if (duo.length == 2) {
+
+                            // Si paire alors reste montrée
+                            if (cards[duo[0]-1].children[1].style.backgroundImage == cards[duo[1]-1].children[1].style.backgroundImage){
+                                paires+=1;
+                                retournees.push(duo[0]);
+                                retournees.push(duo[1]);
+                            } 
+                            // Si pas paire alors on retourne
+                            else   retourneCarte(duo);
+                            
+                            duo = [];
+                        } 
+                    duo.push(carte.id);;                  
+                }
+                // Si WIN :
+                if (retournees.length/2==pairesMax-1 && duo.length==2){                                    
+                    console.log("retournee : "+retournees);
+                    winScreen();
+                }
+            });
+        });
+    }
+
+
+}, false)
+
+
+////////////// FONCTIONS /////////////
+
+function randomAssociations(cards){
+    
+    let liste = {};
+    
+    // Création des urls
+    let urlsName = ["grotte","igloo","maison","musee","palais","royaume","sapin","tour"]
+
+    for (u=0; u<urlsName.length; u++)        urlsName[u] = "url('./../images/png/"+urlsName[u]+".png"
+    urlsName = urlsName.concat(urlsName);
+
+    // Choix random puis associé
+        for (i=0; i<16; i++){
+            a = Math.floor(Math.random() * (urlsName.length - 0) + 0);
+            
+            liste[i] = urlsName[a];
+            
+            // Initialise les backgrounds randoms des cartes
+            cards[i].children[1].style.backgroundImage = urlsName[a];;
+            
+            urlsName.splice(a,1);
+        } 
+}
+
+function retourneCarte(liste){
+    // document.getElementById(liste[0]).style.backgroundImage = "url('./images/motif.webp')";
+    // document.getElementById(liste[1]).style.backgroundImage = "url('./images/motif.webp')";
+
+    document.getElementById(liste[0]).classList.toggle('is-flipped');
+    document.getElementById(liste[1]).classList.toggle('is-flipped');
+}
+
+function winScreen() {
+    let win = document.getElementById('win');
+    console.log(win);
+    win.style.display = "block";
+    
+}
+
